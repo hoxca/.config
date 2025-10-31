1d2586e295461d00fd44ad6ee6a5693657bc5f4e
hugues obolonsky <h.obolonsky@loxodata.com>
2025-10-31 22:22:24 +0100

    add cosmetic lualine

---
+++ nvim/lua/hoxca/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+  local lualine = require("lualine")
+
+  lualine.setup({
+    icons_enabled = true,
+    options = {
+      theme = "gruvbox-material",
+      component_separators = { left = '', right = ''},
+      section_separators = { left = '', right = ''},
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch' },
+      lualine_c = { { 'diff', colored = true, symbols = { added = ' ', modified = ' ', removed = ' ' } }, 'filename' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'encoding' },
+    }
+  })
+  end
+}
+
